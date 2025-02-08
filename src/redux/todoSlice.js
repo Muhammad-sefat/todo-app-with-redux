ad21b6f41af45b8522190fa99a4dabbc3df37e28
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-02-09 01:55:03 +0600

    Final commit

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,7 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const loadTodos = () => {
+  const savedTodos = localStorage.getItem("todos");
+  return savedTodos ? JSON.parse(savedTodos) : [];
+};
+
 const initialState = {
-  todos: [],
+  todos: loadTodos(),
   filter: "all",
 };
 
@@ -10,28 +15,37 @@ export const todoSlice = createSlice({
   initialState,
   reducers: {
     addTodo: (state, action) => {
-      state.todos.push({
+      const newTodo = {
         id: Date.now(),
         text: action.payload,
         completed: false,
-      });
+      };
+      state.todos.push(newTodo);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     updateTodo: (state, action) => {
       const { id, newText } = action.payload;
-      const todo = state.todos.find((todo) => todo.id === id);
-      if (todo) {
-        todo.text = newText;
+      const existingTodo = state.todos.find((todo) => todo.id === id);
+      if (existingTodo) {
+        existingTodo.text = newText;
+        localStorage.setItem("todos", JSON.stringify(state.todos));
       }
-    },
-    toggleTodo: (state, action) => {
-      const todo = state.todos.find((todo) => todo.id === action.payload);
-      if (todo) todo.completed = !todo.completed;
     },
     setFilter: (state, action) => {
       state.filter = action.payload;
+    },
+    toggleTodo: (state, action) => {
+      const existingTodo = state.todos.find(
+        (todo) => todo.id === action.payload
+      );
+      if (existingTodo) {
+        existingTodo.completed = !existingTodo.completed;
+        localStorage.setItem("todos", JSON.stringify(state.todos));
+      }
     },
   },
 });
